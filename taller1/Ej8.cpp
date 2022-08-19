1df9e5e5f8ecf42f3788acf2011aef0ae1058a51
CifuentesJ <juliancifuentesvasquez@gmail.com>
2022-08-19 15:51:21 -0500

    Trabajando en el taller

--- taller1/Ej8.cpp
+++ taller1/Ej8.cpp
@@ -3,3 +3,42 @@
 Donde, 𝑿𝒊 = cada uno de los elementos del vector
 𝑿 = es el promedio del vector y
 𝑵 = el tamaño del vector.*/
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout.setf(ios::fixed);
+    cout.precision(3);
+    srand(time(NULL));
+
+    int N, * vec;
+    double sum = 0, prom = 0, acum = 0, ec;
+
+    cout<< "Ingrese N: ";
+    cin>> N;
+
+    vec = new int[N];
+
+    for(int i = 0; i < N; i++){
+        vec[i] = 100+rand()%900;
+        acum += vec[i];
+    }
+    
+    prom = acum / N;
+    for(int i = 0; i < N; i++){
+        sum += pow((vec[i]-prom),2);
+    }
+    ec = sqrt(sum / (N-1));
+
+    cout<< "\n\nVector: ";
+    for(int i = 0; i < N; i++){
+        cout << "(" << vec[i] << ") "; 
+    }
+    cout<< "\n\nEcuación: " << ec << endl;
+    return 0;
+}
